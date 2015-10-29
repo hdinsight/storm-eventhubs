54b74fff88b115dd8987c2ef2ceabad440d409fa
Chris Shellenbarger <cshellenbarger@yammer-inc.com>
2015-10-29 15:46:12 -0700

    AMQP Metadata - Added AMQP Metadata to the Fields that get emitted as part of the Event Hub Spout.  This allows for bolts to have access to any metadata which were attached to the message rather than just the message itself.

--- src/main/java/com/microsoft/eventhubs/spout/EventDataScheme.java
+++ src/main/java/com/microsoft/eventhubs/spout/EventDataScheme.java
@@ -19,10 +19,14 @@ package com.microsoft.eventhubs.spout;
 
 import backtype.storm.tuple.Fields;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
 import org.apache.qpid.amqp_1_0.client.Message;
 import org.apache.qpid.amqp_1_0.type.Section;
 import org.apache.qpid.amqp_1_0.type.messaging.AmqpValue;
+import org.apache.qpid.amqp_1_0.type.messaging.ApplicationProperties;
 import org.apache.qpid.amqp_1_0.type.messaging.Data;
 
 public class EventDataScheme implements IEventDataScheme {
@@ -31,25 +35,34 @@ public class EventDataScheme implements IEventDataScheme {
 
   @Override
   public List<Object> deserialize(Message message) {
-    List<Object> fieldContents = new ArrayList<Object>();
+    final List<Object> fieldContents = new ArrayList<Object>();
+
+    /**
+     * Default values.
+     */
+    Map metaDataMap = new HashMap();
+    String messageData = "";
 
     for (Section section : message.getPayload()) {
       if (section instanceof Data) {
         Data data = (Data) section;
-        fieldContents.add(new String(data.getValue().getArray()));
-        return fieldContents;
+        messageData = new String(data.getValue().getArray());
       } else if (section instanceof AmqpValue) {
         AmqpValue amqpValue = (AmqpValue) section;
-        fieldContents.add(amqpValue.getValue().toString());
-        return fieldContents;
+        messageData = amqpValue.getValue().toString();
+      } else if(section instanceof ApplicationProperties) {
+        final ApplicationProperties applicationProperties = (ApplicationProperties) section;
+        metaDataMap = applicationProperties.getValue();
       }
     }
 
-    return null;
+    fieldContents.add(messageData);
+    fieldContents.add(metaDataMap);
+    return fieldContents;
   }
 
   @Override
   public Fields getOutputFields() {
-    return new Fields(FieldConstants.Message);
+    return new Fields(FieldConstants.Message, FieldConstants.META_DATA);
   }
 }
